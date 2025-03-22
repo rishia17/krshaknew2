5da10cdf344b98a6b8314ade3bd615a10d24a38f
rishia17 <162045990+rishia17@users.noreply.github.com>
2025-03-22 20:32:40 +0530

    Update chat.tsx

--- frontend/app/chat.tsx
+++ frontend/app/chat.tsx
@@ -31,7 +31,7 @@ export default function ChatScreen() {
 
     try {
       const response = await fetch(
-        'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyBo_ONeMzUZUUdwypHg9eWO5nwndOWc59k',
+        'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=API_KEY',
         {
           method: 'POST',
           headers: {
